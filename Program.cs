159345cd24515942e64cfd0ecb34b8ee2f1718e2
Jackson Wozniak <jjw30@comcast.net>
2025-11-10 17:49:39 -0500

    parse arguments in main method to find command to execute

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
-﻿namespace DevBank;
+﻿using System.Reflection;
+using DevBank.Command;
+using DevBank.Exception;
+
+namespace DevBank;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        
+        var command = args[0].ToLower();
+
+        switch (command)
+        {
+            case "save":
+                SaveCommand.Create().Execute(args);
+                break;
+            case "find":
+                FindCommand.Create().Execute(args);
+                break;
+            case "list":
+                ListCommand.Create().Execute(args);
+                break;
+            case "delete":
+                DeleteCommand.Create().Execute(args);
+                break;
+            case "--version":
+            case "-v":
+                var version = Assembly
+                    .GetExecutingAssembly()
+                    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                    .InformationalVersion ?? "unknown";
+                version = version.Split("+")[0];
+                System.Console.WriteLine($"DevBank version {version}");
+                break;
+            default:
+                System.Console.WriteLine(ExceptionStrings.NoArgumentsException);
+                break;
+        }
     }
 }
---
+++ src/Command/DeleteCommand.cs
@@ -0,0 +1,31 @@
+﻿using DevBank.Console;
+using DevBank.Repository;
+
+namespace DevBank.Command;
+
+public class DeleteCommand : ICommand
+{
+    private readonly IRepository _repository;
+    private readonly IConsole _console;
+
+    protected DeleteCommand(IRepository repository, IConsole console)
+    {
+        _repository = repository;
+        _console = console;
+    }
+
+    public static DeleteCommand Create()
+    {
+        return new DeleteCommand(new JsonRepository(), new SystemConsole());
+    }
+
+    public static DeleteCommand Create(IRepository r, IConsole c)
+    {
+        return new DeleteCommand(r, c);
+    }
+
+    public void Execute(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+}
