18639d1ed7fb59e54e4e796423da160b9e4e25d4
Jackson Wozniak <jjw30@comcast.net>
2026-03-05 21:39:27 -0500

    add debug specific data folder

--- src/DevNote.Shared/Helpers/JsonRepositoryHelper.cs
+++ src/DevNote.Shared/Helpers/JsonRepositoryHelper.cs
@@ -6,13 +6,21 @@ public static class JsonRepositoryHelper
     
     private static string GetAppDataPath()
     {
+        string fileName = "entries.json";
+        string path;
+        
+#if DEBUG
+        string projectRoot = Directory.GetParent(AppContext.BaseDirectory)!.Parent!.Parent!.Parent!.FullName;
+        path = Path.Combine(projectRoot, "App_Data");
+#else
         string path = Path.Combine(
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             "DevNote",
             "Data");
+#endif
         
         Directory.CreateDirectory(path);
-        string filePath = Path.Combine(path, "entries.json");
+        string filePath = Path.Combine(path, fileName);
         if (!File.Exists(filePath))
         {
             File.WriteAllText(filePath, "");
